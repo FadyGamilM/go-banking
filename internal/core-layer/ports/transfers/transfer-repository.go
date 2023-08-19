8ca0b7a89b39e23431f7abc498e86292311532fb
FadyGamilM <1700961@eng.asu.edu.eg>
2023-08-19 14:58:26 +0300

    FEATURE | structure the repo of transfers

--- internal/core-layer/ports/transfers/transfer-repository.go
+++ internal/core-layer/ports/transfers/transfer-repository.go
@@ -1,3 +1,9 @@
 package transfers
 
-type TransferRepository interface{}
+import "gobanking/internal/core-layer/domain"
+
+type TransferRepository interface {
+	Create(*domain.Transfer) (*domain.Transfer, error)
+	GetAll() ([]*domain.Transfer, error)
+	GetByID(int64) (*domain.Transfer, error)
+}
--- internal/data-layer/postgres/transfer-repo.go
+++ internal/data-layer/postgres/transfer-repo.go
@@ -1,1 +1,27 @@
 package postgres
+
+import "gobanking/internal/infra-layer/db/postgres"
+
+type PG_TransferRepository struct {
+	pg *postgres.PG_DB
+}
+
+func NewPG_TransferRepo(pg *postgres.PG_DB) *PG_TransferRepository {
+	return &PG_TransferRepository{
+		pg: pg,
+	}
+}
+
+const (
+	create_Transfer_Query = `
+		INSERT INTO transfers
+		(to_account, from_account, amount)
+		VALUES
+		($1, $2, $3)
+		RETURNING id, to_account, from_account, amount, created_at, updated_at
+	`
+
+	get_All_Transfers_Query = `
+		SELECT id, to_account, from_account, amount, created_at, updated_at
+	`
+)
