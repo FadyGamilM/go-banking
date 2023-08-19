0fd97ae92d82ae6137398bf5d5fe61e4a292eda5
FadyGamilM <1700961@eng.asu.edu.eg>
2023-08-19 11:40:21 +0300

    Finish entry-resource crud unit tests

--- internal/data-layer/postgres/entry-repo_test.go
+++ internal/data-layer/postgres/entry-repo_test.go
@@ -18,10 +18,10 @@ func createUserForTest(req_args *domain.Account) (*domain.Account, error) {
 	return created_acc, err
 }
 
-func TestCreateEntry(t *testing.T) {
+func TestCreateEntry_ForBalanceIncrease(t *testing.T) {
 	// teardown the database content to start fresh :D
-	test_PG_DB.DB.Exec(`DELETE FROM accounts`)
 	test_PG_DB.DB.Exec(`DELETE FROM entries`)
+	test_PG_DB.DB.Exec(`DELETE FROM accounts`)
 
 	// first we create an account with initial balance
 	acc_args := domain.Account{
@@ -46,24 +46,64 @@ func TestCreateEntry(t *testing.T) {
 	require.NotZero(t, created_entry.ID)
 	require.NotZero(t, created_entry.CreatedAt)
 	require.NotZero(t, created_entry.UpdatedAt)
+}
+
+func TestCreateEntry_ForBalanceDecrease(t *testing.T) {
+	// teardown the database content to start fresh :D
+	test_PG_DB.DB.Exec(`DELETE FROM entries`)
+	test_PG_DB.DB.Exec(`DELETE FROM accounts`)
+
+	// first we create an account with initial balance
+	acc_args := domain.Account{
+		OwnerName: "Hossam",
+		Balance:   float64(150.00),
+		Currency:  "USD",
+	}
+	created_acc, err := createAccountForTest(&acc_args)
+	require.NoError(t, err)
 
 	// create a new entry in case user need to withdraw from his account (this will test the entry and update account both !)
 	subtractionEntryBalance := float64(-350.00)
-	entry_args = domain.Entry{
+	entry_args := domain.Entry{
 		AccountID: created_acc.ID,
 		Amount:    subtractionEntryBalance,
 	}
-	created_entry, err = createEntryForTest(&entry_args)
+	created_entry, err := createEntryForTest(&entry_args)
 	require.NoError(t, err)
-	updated_acc, err = test_acc_repo.UpdateByID(created_acc.ID, created_entry.Amount)
+	updated_account, err := test_acc_repo.UpdateByID(created_acc.ID, created_entry.Amount)
 	require.Error(t, err)
-	// require.Equal(t, updated_acc.Balance, created_acc.Balance+created_entry.Amount) // + because we the amoutn is -ve
-	// require.NotZero(t, created_entry.ID)
-	// require.NotZero(t, created_entry.CreatedAt)
-	// require.NotZero(t, created_entry.UpdatedAt)
+	require.Nil(t, updated_account)
 
 }
 
 func TestGetAccountEntries(t *testing.T) {
+	// teardown the database content to start fresh :D
+	test_PG_DB.DB.Exec(`DELETE FROM entries`)
+	test_PG_DB.DB.Exec(`DELETE FROM accounts`)
 
+	acc_args := domain.Account{
+		OwnerName: "Hossam",
+		Balance:   float64(150.00),
+		Currency:  "USD",
+	}
+	created_acc, err := createAccountForTest(&acc_args)
+	require.NoError(t, err)
+
+	additionEntryBalance := float64(+200.00)
+	entry_args := domain.Entry{
+		AccountID: created_acc.ID,
+		Amount:    additionEntryBalance,
+	}
+	created_entry, err := createEntryForTest(&entry_args)
+	require.NoError(t, err)
+
+	// i assume the creation is tested successffully ..
+
+	retrived_entry, err := test_entry_repo.GetOne(created_entry.ID, created_entry.AccountID)
+	require.NoError(t, err)
+
+	require.Equal(t, created_entry.ID, retrived_entry.ID)
+	require.Equal(t, created_entry.Amount, retrived_entry.Amount)
+	require.Equal(t, created_entry.AccountID, retrived_entry.AccountID)
+	require.Equal(t, created_acc.ID, retrived_entry.AccountID)
 }
